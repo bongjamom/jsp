10fb8688d25fc7ce163f16f4da4522ecc3f47220
hyunmin <parkhm323@gmail.com>
2023-05-17 18:08:37 +0900

    first commit

---
+++ src/main/java/com/example/jsp/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.jsp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.jsp.service.HomeService;
+
+@Controller
+public class HomeController {
+  @Autowired
+  private HomeService homeService;
+
+  @GetMapping("/")
+  public String home(Model model) {
+    model.addAttribute("name", "myName");
+    model.addAttribute("time", homeService.getTime());
+    return "index";
+  }
+
+  @GetMapping("list")
+  public void getList(Model model) {
+    model.addAttribute("list", homeService.getList());
+  }
+}
